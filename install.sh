ee905766aded0e727e2173c4c16806f76c1b8ddc
Pavel Zdeněk <pavel-zdenek@users.noreply.github.com>
2025-04-05 23:45:38 +0200

    Add codegen explanation to install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ echo "🚀 Starting project installation..."
 echo "📦 Installing dependencies..."
 npm install || { echo "❌ Error: Failed to install dependencies"; exit 1; }
 
+# The intent was to generate as build step, not even commit the generated code, as it should be done.
+# But the generated code uses module syntax "bundle" = without js/ts suffixes. Which is not compatible with the main app's module syntax "nodenext".
+# Maybe someone will have time to figure out the compatibility.
+# But it's not a blocker because Redmine API will surely not change in the future,
+# and Toggl OpenAPI is downloaded locally anyway because the original is horribly broken.
 #echo "🔧 Generating API clients..."
 #npx @hey-api/openapi-ts -i https://raw.githubusercontent.com/d-yoshi/redmine-openapi/refs/heads/main/openapi.yml -o src/api-redmine -c @hey-api/client-fetch
 #npx @hey-api/openapi-ts -i ./toggl-api-v9.json -o src/api-toggl -c @hey-api/client-fetch
